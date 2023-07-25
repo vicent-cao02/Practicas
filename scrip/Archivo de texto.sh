c6428ff9bc168b891f82bbb1389f7296cbb71c92
vicent-cao02 <139924476+vicent-cao02@users.noreply.github.com>
2023-07-25 17:51:28 -0400

    Add files via upload

---
+++ scrip/Archivo de texto.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd Escritorio/proyecto\ cerveza/
+
+ ls
+
+    fotos
+    otros
+    Practicas-1
+    proyect.ipynb
+    scrip
+    Vicente_Cao
+    proyect
+    Cerveza.zip
+    informe
+    Practicas
+    presentacion
+    proyecto.json
+    texput.log
+
+cd scrip/
+
+ls
+
+    Archivo_de_texto.sh
+    Informe_.pdf
+    presentacion.pdf
+    texput.log
+
+evince presentacion\ .pdf
